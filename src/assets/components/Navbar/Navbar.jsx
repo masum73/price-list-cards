469cc59333a3a5442a83b41ea026aa76b849f36b
Masum Rahman <masumrahman73@gmail.com>
2023-04-04 21:05:11 +0600

    responsive navbar done

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const routes = [
         {
             id: 1,
@@ -29,10 +32,20 @@ const Navbar = () => {
         }
     ];
     return (
-        <nav>
-            <ul>
+        <nav className='bg-purple-400'>
+            <div onClick={() => setOpen(!open)} className='md:hidden'>
+                {/* <span>{open === true ? 'open' : 'close'}</span> */}
+                <span>{open === true ?
+                    <XMarkIcon className="h-6 w-6 text-purple-500" />
+                    : <Bars3Icon className="h-6 w-6 text-purple-500" />
+                }</span>
+            </div>
+            <ul className={`md:flex absolute md:static duration-500 bg-purple-400 pl-8 py-2 ${open ? 'top-6' : '-top-36'}`}>
                 {
-                    routes.map(route => <li>{route.name}</li>)
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
                 }
             </ul>
         </nav>
