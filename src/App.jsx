1e94ddd8168c20e7152db31c5c4d17dea5be92c4
Masum Rahman <masumrahman73@gmail.com>
2023-04-07 23:26:17 +0600

    axios installed done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './assets/components/Navbar/Navbar'
 import PriceList from './assets/components/PriceList/PriceList'
+import Dashboard from './assets/components/Dashboard/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,26 +14,8 @@ function App() {
       <Navbar></Navbar>
       <h1 className='text-7xl text-purple-500'>Hello from tailwind</h1>
       <PriceList></PriceList>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <Dashboard></Dashboard>
+      
     </div>
   )
 }
